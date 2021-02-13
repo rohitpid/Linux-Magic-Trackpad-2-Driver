9ffcb9f3e9d812345750d8e6732002175071bca6
RicardoEPRodrigues <ricardo.e.p.rodrigues@gmail.com>
2021-02-13 21:12:47 +0000

    Improve Driver Loading through Modprobe and DKMS
    This update simplifies the code. Please run `remove.sh` before updating the driver to ensure a smooth transition.
    
    Removed unnecessary files from repo.
    
    Changes were made to install, the main loading script to make use of modprobe more accurately.
    
    The parameters for hid-magicmouse were moved to /etc/modprobe.d/hid-magicmouse.conf . This allows the unloading and loading of the module with predefined parameters.
    
    A `remove.sh` script was added to uninstall the driver. NOTE: It does not remove the bluetooth fix.
    
    Changed DKMS version to reflect the driver name.

--- install.sh
+++ install.sh
@@ -9,36 +9,29 @@ set -e
 set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-OPT="/opt/magic-mouse-fix"
-UDEV="/etc/udev/rules.d"
+OPT_DIR="/opt/magic-mouse-fix"
+UDEV_DIR="/etc/udev/rules.d"
+MODPROBE_DIR="/etc/modprobe.d"
+
+
+# Copy Modprobe config file
+cp -f ${DIR}${MODPROBE_DIR}/hid-magicmouse.conf ${MODPROBE_DIR}/hid-magicmouse.conf
 
 # Install drive through DKMS
 chmod u+x ${DIR}/scripts/install.sh
 ${DIR}/scripts/install.sh
 
-# Build drive to be used for the Bluetooth fix
-cd ${DIR}/linux/drivers/hid
-make clean
-make
-cd ${DIR}
-
-# Copy `.ko` and script to activate it to OPT directory
-mkdir -p ${OPT}
-cp -f ${DIR}/linux/drivers/hid/hid-magicmouse.ko ${OPT}/hid-magicmouse.ko
-cp -f ${DIR}/scripts/magic-mouse-2-add.sh ${OPT}/magic-mouse-2-add.sh
-chmod +x ${OPT}/magic-mouse-2-add.sh
+# Copy script to load the driver to OPT directory
+mkdir -p ${OPT_DIR}
+cp -f ${DIR}/scripts/magic-mouse-2-add.sh ${OPT_DIR}/magic-mouse-2-add.sh
+chmod +x ${OPT_DIR}/magic-mouse-2-add.sh
 
 # Copy udev rule and reload udev
-cp -f ${DIR}/udev/rules.d/10-magicmouse.rules ${UDEV}/10-magicmouse.rules
+cp -f ${DIR}${UDEV_DIR}/10-magicmouse.rules ${UDEV_DIR}/10-magicmouse.rules
 udevadm control -R
 
 # Disable eSCO mode in Bluetooth to fix disconnection problems with the mouse
 echo 1 | tee /sys/module/bluetooth/parameters/disable_esco
 systemctl restart bluetooth
-# persist setting
+# persist eSCO mode in Bluetooth setting
 echo "options bluetooth disable_esco=1" | tee /etc/modprobe.d/bluetooth-tweaks.conf
-
-# Clean driver build
-cd ${DIR}/linux/drivers/hid
-make clean
-cd ${DIR}
--- remove.sh
+++ remove.sh
@@ -7,10 +7,14 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 OPT="/opt/magic-mouse-fix"
 UDEV="/etc/udev/rules.d"
 
+# Remove drive through DKMS
+chmod u+x ${DIR}/scripts/remove.sh
+${DIR}/scripts/remove.sh
+
 # Copy `.ko` and script to activate it to OPT directory
 rm -rf ${OPT}
 
 # Remove the udev rule and reload udev
-rm ${UDEV}/10-magicmouse.rules
+rm -f ${UDEV}/10-magicmouse.rules
 udevadm control -R
-/etc/init.d/bluetooth restart
+systemctl restart bluetooth
